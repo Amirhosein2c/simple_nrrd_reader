de4eeeacd7bc8e142fac870ea808261d38204a93
amir <amirhosein.toosi@gmail.com>
2021-04-21 16:46:06 +0200

    Switched from PIL to OPENCV to make sure of loseless JPEG

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import nrrd
-from PIL import Image
+# from PIL import Image
+import cv2
 
 filename = "./Guenette_FN_CISS_Subject_09.nrrd"
 filedata, fileheader = nrrd.read(filename)
@@ -8,13 +9,13 @@ print("fileheader", fileheader)
 print("filedata", filedata.shape)
 
 for i in range(filedata.shape[2]):
-    slice = filedata[:,:,i]
-    print(type(slice))
-    print(slice.shape)
-    slice = (slice.astype(np.float64)-slice.min())*255.0 / (slice.max()-slice.min())
-    im = Image.fromarray(slice)
-    im.show()
-    im = im.convert("L")
-    # im.save("./slice_" + str(i).zfill(3)+ ".png")
-    im.save("slice_" + str(i).zfill(3)+ ".jpg", quality=95, subsampling=0)
-    print(type(im))
+    _slice = filedata[:,:,i]
+    print(type(_slice))
+    print(_slice.shape)
+    slice = (_slice.astype(np.float64)-_slice.min()) / (_slice.max()-_slice.min())
+    slice = slice.astype('float32')
+    cv2.imshow("raw image", slice)
+    cv2.waitKey(30)
+    cv2.imwrite("slice_" + str(i).zfill(3)+ ".jpg", slice*255, [cv2.IMWRITE_JPEG_QUALITY, 100])
+    # cv2.imwrite("slice_" + str(i).zfill(3)+ ".png", slice*255)
+cv2.destroyAllWindows
