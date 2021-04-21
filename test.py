259c38ff4fff824a17a88d290905b1abbba07611
amir <amirhosein.toosi@gmail.com>
2021-04-21 15:06:59 +0200

    JPEG write bug fixed

--- test.py
+++ test.py
@@ -11,23 +11,10 @@ for i in range(filedata.shape[2]):
     slice = filedata[:,:,i]
     print(type(slice))
     print(slice.shape)
-    # im = Image.fromarray(np.uint8(slice))
     slice = (slice.astype(np.float64)-slice.min())*255.0 / (slice.max()-slice.min())
     im = Image.fromarray(slice)
     im.show()
+    im = im.convert("L")
+    # im.save("./slice_" + str(i).zfill(3)+ ".png")
     im.save("slice_" + str(i).zfill(3)+ ".jpg", quality=95, subsampling=0)
     print(type(im))
-
-
-
-# datashape = fileheader{}
-
-
-
-
-
-# data = np.linspace(1, 50, 50)
-# nrrd.write('output.nrrd', data)
-
-# data2, header = nrrd.read('output.nrrd')
-# print(np.all(data == data2))
